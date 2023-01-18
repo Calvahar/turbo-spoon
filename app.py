f2aabd03fd6bfb0255cb6086fb347babff79331b
Calvahar <58914729+Calvahar@users.noreply.github.com>
2023-01-19 00:52:34 +0100

    modified:   app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World! (DIT IS DE UPDATE) Server uptime: (up %s)\n" % elapsed()
+    return "Hello World! Server uptime: (up %s)\n" % elapsed()
 
 
 if __name__ == "__main__":
