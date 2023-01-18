8f660568c8141a4608a942966a307f32a883f2f6
Calvahar <58914729+Calvahar@users.noreply.github.com>
2023-01-18 10:42:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 
 START = time.time()
 
+
 def elapsed():
     running = time.time() - START
     minutes, seconds = divmod(running, 60)
     hours, minutes = divmod(minutes, 60)
     return "%d:%02d:%02d" % (hours, minutes, seconds)
 
+
 @app.route('/')
 def root():
-    return "Hello World (Hoi dit is een andere test)! (up %s)\n" % elapsed()
+    return "Hello World (Hoi dit is een andere test)! Server uptime: (up %s)\n" % elapsed()
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
